2b393ac0d47c9f10aeaf48c3c052e1c4c25af8e6
wldus1208 <ji915405@gmail.com>
2024-03-20 10:31:27 +0900

    bootstarp

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+// src/AlbumPage.js
+
+import React from 'react';
+import Card from './Card'; // Card 컴포넌트의 경로가 올바른지 확인하세요.
+
+const AlbumPage = () => {
+  const cards = Array.from({ length: 9 }); // 9개의 카드를 생성하기 위한 배열
+
+  return (
+    <div className="album py-5 bg-body-tertiary">
+      <div className="container">
+        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          {cards.map((_, index) => (
+            <Card key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AlbumPage;
---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+// About.js
+
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>About 페이지</h2>
+      <p>이곳은 About 페이지입니다.</p>
+    </div>
+  );
+}
+
+export default About;
