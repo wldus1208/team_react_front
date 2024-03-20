3d4eaa62d0a75a5f2acee38a84cf2148786b083e
wldus1208 <ji915405@gmail.com>
2024-03-20 11:42:18 +0900

    loginform

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './pages/About';
+import Login from './pages/LoginForm';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter> 
     </div>
