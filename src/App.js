8feb385aa0ce25b5cd1154f7a289775a8d2fd6fe
MHJ <picpat0418@gmail.com>
2024-03-20 11:22:04 +0900

    modified:   src/App.js 	new file:   src/pages/Test.jsx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Header from './components/Header';
 import Home from './components/Home';
 import About from './pages/About';
+import Test from './pages/Test';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter> 
     </div>
